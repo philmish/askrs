08b90d75afcfde7484ecc5b30a8fa47e1f8c3164
philmish <philmish.dev@gmail.com>
2025-09-13 09:49:07 +0000

    format cli code

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,5 +1,5 @@
-use clap::Parser as clapParser;
 use clap::ArgAction;
+use clap::Parser as clapParser;
 use dns::record::RecordType;
 use parsing::byte_stream_parser::ByteStreamParser;
 use parsing::Query;
@@ -13,7 +13,6 @@ pub mod socket;
 #[clap(version = "0.1")]
 #[clap(about = "CLI tool for requesting dns records.", long_about = None)]
 struct Flags {
-
     /// Target adress or domain to request records for.
     #[clap(short, long)]
     uri: String,
@@ -36,13 +35,12 @@ struct Flags {
 }
 
 impl Flags {
-
     pub fn get_rtype(&self) -> RecordType {
-        return RecordType::from_string(self.record.clone())
+        return RecordType::from_string(self.record.clone());
     }
 
     pub fn get_server(&self) -> DNSSocket {
-        return DNSSocket::from_string(&self.server);
+        DNSSocket::from_string(&self.server)
     }
 }
 
@@ -51,36 +49,28 @@ pub struct CLI {
 }
 
 impl CLI {
-
     pub fn init() -> Self {
         let flags = Flags::parse();
-        return Self{
-            flags,
-        };
+        Self { flags }
     }
 
     fn send_query(&self, q: Query, srv: DNSSocket, verbose: bool) -> Vec<u8> {
-        let client = socket::UDPClient{};
+        let client = socket::UDPClient {};
         let msg = q.to_bytes();
-        let a = client.send_and_recieve(msg, srv)
-                      .unwrap();
+        let a = client.send_and_recieve(msg, srv).unwrap();
         if verbose {
             q.print(verbose);
         }
-        return a;
+        a
     }
 
     pub fn run(&self) {
         let qry = Query::new(
             self.flags.uri.clone(),
             self.flags.get_rtype(),
-            self.flags.rd.clone(),
+            self.flags.rd,
         );
-        let a = self.send_query(
-            qry,
-            self.flags.get_server(),
-            self.flags.verbose
-        );
+        let a = self.send_query(qry, self.flags.get_server(), self.flags.verbose);
         let resp = ByteStreamParser::new(&a).parse_response().unwrap();
         resp.print(self.flags.verbose);
     }
--- cli/src/socket.rs
+++ cli/src/socket.rs
@@ -1,4 +1,7 @@
-use std::{net::{UdpSocket, SocketAddr, SocketAddrV4, Ipv4Addr}, time::Duration};
+use std::{
+    net::{Ipv4Addr, SocketAddr, SocketAddrV4, UdpSocket},
+    time::Duration,
+};
 
 use utility::Blob;
 
@@ -11,15 +14,11 @@ pub enum DNSSocket {
 impl DNSSocket {
     fn get_ip(&self) -> SocketAddr {
         match self {
-            DNSSocket::GOOGLE => SocketAddr::V4(
-                SocketAddrV4::new(Ipv4Addr::new(8,8,4,4), 53)
-            ),
-            DNSSocket::CLOUDFLARE => SocketAddr::V4(
-                SocketAddrV4::new(Ipv4Addr::new(1,1,1,1), 53)
-            ),
-            DNSSocket::QUAD9 => SocketAddr::V4(
-                SocketAddrV4::new(Ipv4Addr::new(9, 9, 9, 9), 53)
-            ),
+            DNSSocket::GOOGLE => SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(8, 8, 4, 4), 53)),
+            DNSSocket::CLOUDFLARE => {
+                SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(1, 1, 1, 1), 53))
+            }
+            DNSSocket::QUAD9 => SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(9, 9, 9, 9), 53)),
         }
     }
 
@@ -28,32 +27,33 @@ impl DNSSocket {
             "google" => DNSSocket::GOOGLE,
             "cloudflare" => DNSSocket::CLOUDFLARE,
             "quad9" => DNSSocket::QUAD9,
-            _ => DNSSocket::GOOGLE
+            _ => DNSSocket::GOOGLE,
         }
     }
 }
 
-pub struct UDPClient{}
+pub struct UDPClient {}
 impl UDPClient {
-
     pub fn send_and_recieve(&self, msg: Vec<u8>, server: DNSSocket) -> Result<Vec<u8>, String> {
-        let msg_bytes = msg.to_socket_msg()
-                           .expect("Failed to parse msg to socket data.");
+        let msg_bytes = msg
+            .to_socket_msg()
+            .expect("Failed to parse msg to socket data.");
 
-        let socket = UdpSocket::bind(
-            (Ipv4Addr::UNSPECIFIED, 0)
-        ).expect("Failed to initialize udp socket.");
+        let socket =
+            UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0)).expect("Failed to initialize udp socket.");
         println!("Socket bound to local address 0.0.0.0:0");
 
-        socket.set_read_timeout(
-            Some(Duration::from_secs(5))
-        ).expect("Failed to set socket read time out");
-        socket.connect(
-            server.get_ip()
-        ).expect("Failed to connect to DNS socket.");
+        socket
+            .set_read_timeout(Some(Duration::from_secs(5)))
+            .expect("Failed to set socket read time out");
+        socket
+            .connect(server.get_ip())
+            .expect("Failed to connect to DNS socket.");
 
-        let mut buf  = vec![0;4096];
-        let _send_bytes = socket.send(&msg_bytes).expect("Failed to send message over socket.");
+        let mut buf = vec![0; 4096];
+        let _send_bytes = socket
+            .send(&msg_bytes)
+            .expect("Failed to send message over socket.");
         let length: usize = match socket.recv(&mut buf) {
             Ok(recieved) => recieved,
             Err(_) => 0,
