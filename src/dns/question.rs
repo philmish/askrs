7c5feda5b7e22f342b072fbd5893986d94b0baeb
philmish <philmish.dev@gmail.com>
2025-11-29 19:05:30 +0100

    improve: add constructor for A Type Inet Query Question

--- src/dns/question.rs
+++ src/dns/question.rs
@@ -1,9 +1,9 @@
 use byters::{BigEndian, ReadsIntoBytes};
 
 use crate::dns::{
-    class::QClass,
+    class::{Class, QClass, RecordClass},
     name::{Label, Name, NameError},
-    r#type::QType,
+    r#type::{QType, RecordType},
 };
 
 pub struct Question<'a> {
@@ -12,7 +12,15 @@ pub struct Question<'a> {
     qclass: QClass,
 }
 
-impl Question<'_> {
+impl<'a> Question<'a> {
+    pub fn a_type_record(name: Name<'a>) -> Self {
+        Self {
+            name,
+            qtype: QType::from(RecordType::A),
+            qclass: QClass::from(RecordClass::IN),
+        }
+    }
+
     pub fn try_to_bytes(&self) -> Result<Vec<u8>, NameError> {
         let name_labels: Vec<Label> = self.name.try_into()?;
         let mut name_bytes: Vec<u8> = name_labels
@@ -20,6 +28,11 @@ impl Question<'_> {
             .map(|l| l.as_bytes())
             .collect::<Vec<&[u8]>>()
             .concat();
+        println!(
+            "got {} bytes from {} labels",
+            name_bytes.len(),
+            name_labels.len()
+        );
         let qt_bytes = BigEndian::read_from_u16(self.qtype.into()).to_vec();
         let qc_bytes = BigEndian::read_from_u16(self.qclass.into()).to_vec();
         Ok([name_bytes, qt_bytes, qc_bytes].concat())
