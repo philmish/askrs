95941fa5627fb36d431e829e5f31395b3247c10f
philmish <philmish.dev@gmail.com>
2025-11-29 19:04:54 +0100

    improve: add DnsError for invalid domain names

--- src/dns/error.rs
+++ src/dns/error.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use crate::dns::header::ErrorCode;
+use crate::dns::{header::ErrorCode, name::NameError};
 
 #[derive(Debug)]
 pub enum DnsError {
@@ -11,7 +11,10 @@ pub enum DnsError {
     InvalidQClass(u16),
     InvalidHeaderLength(usize),
     QueryError(ErrorCode),
+    InvalidDomainName(NameError),
 }
+
+impl std::error::Error for DnsError {}
 
 impl Display for DnsError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -19,4 +22,8 @@ impl Display for DnsError {
     }
 }
 
-impl std::error::Error for DnsError {}
+impl From<NameError> for DnsError {
+    fn from(value: NameError) -> Self {
+        DnsError::InvalidDomainName(value)
+    }
+}
