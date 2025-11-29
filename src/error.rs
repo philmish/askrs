9478b375908204d0cf5bdadd6042004145d1a979
philmish <philmish.dev@gmail.com>
2025-11-29 19:03:43 +0100

    improve: add more tranform trait impls for Askrs error

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,9 @@
+use std::fmt::Display;
+
 use crate::{
     client::ClientError,
     dns::{error::DnsError, name::NameError},
 };
-
-pub type AskrsResult<T> = Result<T, AskrsError>;
 
 #[derive(Debug)]
 pub enum AskrsError {
@@ -12,8 +12,24 @@ pub enum AskrsError {
     ClientError(ClientError),
 }
 
+pub type AskrsResult<T> = Result<T, AskrsError>;
+
+impl std::error::Error for AskrsError {}
+
+impl Display for AskrsError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 impl From<ClientError> for AskrsError {
     fn from(value: ClientError) -> Self {
         AskrsError::ClientError(value)
+    }
+}
+
+impl From<NameError> for AskrsError {
+    fn from(value: NameError) -> Self {
+        AskrsError::DomainNameError(value)
     }
 }
